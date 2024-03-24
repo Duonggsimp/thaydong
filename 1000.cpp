43da6389a4b210d4e8540e57225e81a7b96c790c
Duonggsimp <152135459+Duonggsimp@users.noreply.github.com>
2024-03-24 20:35:30 +0700

    Create 1000.cpp

---
+++ 1000.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fi first
+#define se second
+#define xn '\n'
+using namespace std;
+const int N = 1e6+5;
+const ll mo = 1e9+7;
+
+ll a,b; 
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+   cin >> a >> b; 
+   cout << a+b; 
+  
+    return 0;
+}
