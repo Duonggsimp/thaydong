7d34c0dbbf170cf1d39043e1615a4945695f0d24
Duonggsimp <152135459+Duonggsimp@users.noreply.github.com>
2024-04-02 16:40:37 +0700

    Create 1414.cpp

---
+++ 1414.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fi first
+#define se second
+#define xn '\n'
+using namespace std;
+const int N = 1e6+5;
+const ll mo = 1e9+7;
+
+ll t;
+ll n;
+ll a[N];
+
+void sng(){
+  for (long i=1; i<=N; i++){
+    for (long j=i; j<=N; j+=i)
+        a[j] ++;
+  }
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    sng();
+
+    cin >> t;
+    while (t--){
+        cin >> n;
+        cout << a[n] << xn;
+    }
+    return 0;
+}
