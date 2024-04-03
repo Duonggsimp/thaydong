e222e1d52cecd85ed3c0c3e3c1e861b0c0c45136
Duonggsimp <152135459+Duonggsimp@users.noreply.github.com>
2024-04-03 20:42:15 +0700

    Create 1419.cpp

---
+++ 1419.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fi first
+#define se second
+#define xn '\n'
+using namespace std;
+const int N = 1e6+5;
+const ll mo = 1e9+7; 
+
+ll t; 
+ll a,b; 
+
+int main(){ 
+    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    cin >> t; 
+    while (t--){
+        cin >> a >> b; 
+        cout << a*b/__gcd(a,b) << xn; 
+    }
+    return 0; 
+}
