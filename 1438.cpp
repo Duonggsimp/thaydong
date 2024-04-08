30d6938088bae6c614a76a52bbeaf713b5c15ea3
Phung Tan Dai Duong <152135459+Duonggsimp@users.noreply.github.com>
2024-04-08 15:54:41 +0700

    Create 1438.cpp

---
+++ 1438.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fi first
+#define se second
+#define xn '\n'
+using namespace std;
+const int N = 1e6+5;
+const ll mo = 1e9+7;
+
+ll n,m;
+ll res;
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n;
+    m = n;
+    while (n != 0){
+        ll tmp = n % 10;
+        if (tmp != 0 && m % tmp == 0) res++;
+        n/=10;
+    }
+
+    cout << res;
+    return 0;
+}
+
